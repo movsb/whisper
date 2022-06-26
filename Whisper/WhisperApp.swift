f08f2618b69ddae05f8f46a56f2334b57b715393
movsb <chkesp@gmail.com>
2022-06-26 18:17:23 +0800

    Initial Commit

---
+++ Whisper/WhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperApp.swift
+//  Whisper
+//
+//  Created by Yang Tao on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
